2153e460dd46ce92829cb3675f07162855fc02a1
mikosamanda <amanda_mikos@hotmail.com>
2024-01-22 20:47:17 -0300

    esqueleto do projeto

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
-function handleColorChange(){
-    var colorValue = 50
-    document.getElementsByClassName("R")
+
+function handleColorChange(event, color){
+    var unitpixel = document.getElementById(color)
+    unitpixel.style.filter = `brightness(${event.target.value}%)`
+
+    var pixel = document.getElementById('pixel')
+    var R = document.getElementById('R').value * 2.55
+    var G = document.getElementById('G').value * 2.55
+    var B = document.getElementById('B').value * 2.55
+    pixel.style.backgroundColor = `rgb(${R}, ${G}, ${B})`
 }
+
